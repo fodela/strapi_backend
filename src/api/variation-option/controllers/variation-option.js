4f26922a828286ca1ea96c4311cf8c0b021b0f4e
fodela <particleme30@gmail.com>
2024-02-19 06:16:06 +0000

    feat: add relevant tables and relationships

---
+++ src/api/variation-option/controllers/variation-option.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * variation-option controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::variation-option.variation-option');
---
+++ src/api/variation-option/routes/variation-option.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * variation-option router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::variation-option.variation-option');
